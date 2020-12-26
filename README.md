d3d6d22e23fc6f5ae72ef9322bc2aa72de6026ae
Stanislav Semenchuk <undefy23@gmail.com>
2020-12-27 00:14:06 +0300

    set up webpack for react

--- README.md
+++
@@ -1,4 +0,0 @@
-# client-form
-
-Hello! I've never used Vue before and some pieces of the code are quite messy. If I have a little
-more time I could do it better properly using the oportunities that Vue provides.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebPackPlugin({ template: './src/index.html' })]
+}
